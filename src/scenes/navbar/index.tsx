d8a32155ae41f1aff680740b15c526bc6bbb820c
Haziq Naeem <haziqnaeem36@gmail.com>
2023-03-04 10:35:38 -0500

    configure tailwind to work correctly

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,3 +1,4 @@
+import '../../index.css'
 import { useState } from "react"
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 import Logo from '../../assets/Logo.png'
@@ -12,7 +13,18 @@ const Navbar = (props: Props) => {
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             <img src={Logo} alt="logo" />
-            {/* <div className={`${flexBetween} `}></div> */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <p>Home</p>
+                <p>Bnefits</p>
+                <p>Classes</p>
+                <p>Contact Us</p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a member</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
