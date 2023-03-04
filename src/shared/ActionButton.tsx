f89e4322ca955e73712b5d6dc5b11928c36ef138
Haziq Naeem <haziqnaeem36@gmail.com>
2023-03-04 11:36:29 -0500

    complete smaller screen navbar display

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { SelectedPage } from "./types"
 import AnchorLink from "react-anchor-link-smooth-scroll"
 
 type Props = {
@@ -8,7 +9,11 @@ type Props = {
 
 const ActionButton = ({children, setSelectedPage}: Props) => {
   return (
-    <AnchorLink>
+    <AnchorLink
+      className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`} 
+    >
       {children} 
     </AnchorLink>
   )
